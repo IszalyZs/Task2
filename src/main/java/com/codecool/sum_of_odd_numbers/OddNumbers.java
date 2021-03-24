b423dcbfcb8a53757a02d1f156632058f15d992e
IszalyZs <iszalyzs@gmail.com>
2021-03-24 15:13:41 +0100

    Add OddNumbers.java

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,14 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        int oddNumbersCounter = 0;
+        for (Integer number : numbers) {
+            if (number % 2 != 0) {
+                oddNumbersCounter++;
+            }
+        }
+        if (oddNumbersCounter == 0) return 0;
+        else return numbers.stream().filter(oddNumbers -> oddNumbers % 2 != 0).mapToInt(Integer::intValue).sum();
+
     }
 }
